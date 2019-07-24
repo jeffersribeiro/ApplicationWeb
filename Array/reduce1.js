fa38620ab5c14f6e791d676586b22ec730e54411
Jefferson Ribeiro <jefferson_ribeiro12345@hotmail.com>
2019-07-24 18:20:53 -0300

    commit

--- Array/reduce1.js
+++ Array/reduce1.js
@@ -1,11 +1,11 @@
 const alunos = [
-    {nome:'Maria', nota: 7.3, bolsista: false},
-    {nome:'Jaão', nota: 9.3, bolsista: true},
-    {nome:'Pedro', nota: 2.3, bolsista: false},
-    {nome:'Ana', nota: 6.3, bolsista: true},
+    {nome:'Maria', nota: 7, bolsista: true},
+    {nome:'Jaão', nota: 9, bolsista: true},
+    {nome:'Pedro', nota: 2, bolsista: false},
+    {nome:'Ana', nota: 6, bolsista: true},
 ]
-const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual){
-    const resultado = acumulador + atual
-    console.log(`${(acumulador).toFixed()} + ${atual} = ${(resultado).toFixed(2)}`)
-    return resultado
-})
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual, indice){
+  return acumulador + atual
+}) // pode-se pasar um valor inicial ao inal do fechamento de chave da funcção callback
+
+console.log(resultado)
---
+++ Array/reduce2.js
@@ -0,0 +1,8 @@
+const alunos = [
+    {nome:'Maria', nota: 2, bolsista: true},
+    {nome:'João', nota: 9, bolsista: true},
+    {nome:'Pedro', nota: 2, bolsista: false},
+    {nome:'Ana', nota: 6, bolsista: false},
+]
+
+//Todos os alunos são Bolsistas?
