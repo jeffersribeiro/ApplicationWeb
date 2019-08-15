0cd4a5b8b3f0a0f736bfa6dfba07aaa45d67d042
Jefferson Ribeiro <jefferson_ribeiro12345@hotmail.com>
2019-08-15 16:44:55 -0300

    commit

--- html/Form/server.js
+++ html/Form/server.js
@@ -6,7 +6,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabens!!</h1>')
+    resp.send('<h1>Parabens, Usuario Alterado!!</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabens, Usuario Alterado!!</h1>')
+
 })
 
 app.listen(3003)
